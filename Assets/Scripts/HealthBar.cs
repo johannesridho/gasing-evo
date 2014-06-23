caf821627267a0c9db4546784255442642367155
13510103 <johan.ridho@gmail.com>
2014-06-24 01:24:31 +0700

    health bar pake tekstur eksternal

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,6 +8,9 @@ public class HealthBar : MonoBehaviour {
 	public Vector3 screenPosition;
 	public float healthBarLength;
 
+	public Texture2D teksturHealth;
+	public Texture2D teksturHealth2;
+
 	void Awake(){
 		if(!gasing){
 			gasing = GetComponent<Gasing>();
@@ -27,21 +30,17 @@ public class HealthBar : MonoBehaviour {
 	}
 
 	void OnGUI(){		
-//		GUIStyle styleBarKosong = new GUIStyle ();
-//		GUISkin skin = new GUISkin ();
-
 		GUIStyle style = new GUIStyle(GUI.skin.box);
-		Texture2D texture = new Texture2D(1, 1);
-		texture.SetPixel (1, 1, Color.green);
-		texture.Apply ();
-		style.normal.background = texture;
+//		Texture2D texture = new Texture2D(1, 1);
+//		texture.SetPixel (1, 1, Color.green);
+//		texture.Apply ();
+		style.normal.background = teksturHealth;
 
 		GUIStyle style2 = new GUIStyle(GUI.skin.box);
-		Texture2D texture2 = new Texture2D(1, 1);
-		texture2.SetPixel (1, 1, Color.red);
-		texture2.Apply ();
-
-		style2.normal.background = texture2;
+//		Texture2D texture2 = new Texture2D(1, 1);
+//		texture2.SetPixel (1, 1, Color.red);
+//		texture2.Apply ();
+		style2.normal.background = teksturHealth2;
 
 		GUI.Box(new Rect(screenPosition.x - 25, screenPosition.y - 35, 50, 7), "",style2);
 		GUI.Box(new Rect(screenPosition.x - 25, screenPosition.y - 35, healthBarLength, 7), "",style);
