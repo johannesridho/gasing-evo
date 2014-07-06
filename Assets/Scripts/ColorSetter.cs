63728977ad749507988cd554595ef419c33bbf13
krisnasambora <krisnasambora@yahoo.com>
2014-07-06 19:31:49 +0700

    Polishing main menu

---
+++ Assets/Scripts/ColorSetter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSetter : MonoBehaviour {
+	public Color objectColor;
+
+	// Use this for initialization
+	void Start () {
+		objectColor = Color.black;
+		renderer.material.color = objectColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
