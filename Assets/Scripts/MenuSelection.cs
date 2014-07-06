9aea929648425522c0a864326bbfebb118dfc589
krisnasambora <krisnasambora@yahoo.com>
2014-07-06 20:11:39 +0700

    menu script refining

--- Assets/Scripts/MenuSelection.cs
+++ Assets/Scripts/MenuSelection.cs
@@ -19,128 +19,11 @@ public class MenuSelection : MonoBehaviour {
 void OnMouseUp(){
 		//atur yang mana yang hilang yang mana yang muncul
 		if (isSinglePlayer) {
-						//show all its children, hide other's children, hide parents' renderer and collider
-						Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer> ();
-						Collider[] colliders  = new Collider[3] {GameObject.Find ("Single Player").GetComponent<Collider>(),
-																GameObject.Find ("Multiplayer").GetComponent<Collider>(),
-																GameObject.Find ("Options").GetComponent<Collider>()};
-						Renderer[] MultiPlayerRenderers = GameObject.Find ("Multiplayer").GetComponentsInChildren<Renderer> ();
-						Renderer[] OptionsRenderers = GameObject.Find ("Options").GetComponentsInChildren<Renderer> ();
-						Collider[] submenucolliders = new Collider[3] {GameObject.Find ("Story Mode").GetComponent<Collider> (),
-							GameObject.Find ("Arcade Mode").GetComponent<Collider> (),
-							GameObject.Find ("Back Button S").GetComponent<Collider> ()};
-						foreach(Collider c in submenucolliders){
-							c.collider.enabled = true;
-						}
-						//disable parents' collider
-						foreach(Collider c in colliders){
-								c.collider.enabled = false;
-						}
-						//enable children's renderer
-						foreach (Renderer r in renderers) {
-								if (r.name == "Single Player") {
-										r.enabled = false;
-								} else {
-										r.enabled = true;
-								}
-						}
-						foreach (Renderer r in MultiPlayerRenderers) {
-								if (r.name == "Multiplayer") {
-										r.enabled = false;
-								} else {
-										r.enabled = false;
-								}
-						}
-						foreach (Renderer r in OptionsRenderers) {
-								if (r.name == "Options") {
-										r.enabled = false;
-								} else {
-										r.enabled = false;
-								}
-						}
+						showSinglePlayerMenu();
 				} else if (isMultiPlayer) {
-						//show all its children, hide other's children
-						Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer> ();
-						Collider[] colliders  = new Collider[3] {GameObject.Find ("Single Player").GetComponent<Collider>(),
-							GameObject.Find ("Multiplayer").GetComponent<Collider>(),
-							GameObject.Find ("Options").GetComponent<Collider>()};
-						Renderer[] SinglePlayerRenderers = GameObject.Find ("Single Player").GetComponentsInChildren<Renderer> ();
-						Renderer[] OptionsRenderers = GameObject.Find ("Options").GetComponentsInChildren<Renderer> ();
-						Collider[] submenucolliders = new Collider[3] {
-							GameObject.Find ("Team Mode").GetComponent<Collider> (),
-							GameObject.Find ("Royal Rumble").GetComponent<Collider> (),
-							GameObject.Find ("Back Button M").GetComponent<Collider> ()};
-						foreach(Collider c in submenucolliders){
-							c.collider.enabled = true;
-						}
-						//disable parents' collider
-						foreach(Collider c in colliders){
-							c.collider.enabled = false;
-						}
-						foreach (Renderer r in renderers) {
-								if (r.name == "Multiplayer") {
-										r.enabled = false;
-								} else {
-										r.enabled = true;
-								}
-						}
-						foreach (Renderer r in SinglePlayerRenderers) {
-								if (r.name == "Single Player") {
-										r.enabled = false;
-								} else {
-										r.enabled = false;
-								}
-						}
-						foreach (Renderer r in OptionsRenderers) {
-								if (r.name == "Options") {
-										r.enabled = false;
-								} else {
-										r.enabled = false;
-								}
-						}
+						showMultiplayerMenu();
 				} else if (isOption) {
-						//show all its children, hide other's children
-						Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer> ();
-						Collider[] colliders  = new Collider[3] {GameObject.Find ("Single Player").GetComponent<Collider>(),
-							GameObject.Find ("Multiplayer").GetComponent<Collider>(),
-							GameObject.Find ("Options").GetComponent<Collider>()};
-						Renderer[] SinglePlayerRenderers = GameObject.Find ("Single Player").GetComponentsInChildren<Renderer> ();
-						Renderer[] MultiPlayerRenderers = GameObject.Find ("Multiplayer").GetComponentsInChildren<Renderer> ();
-//						Collider[] submenucolliders = new Collider[5] {GameObject.Find ("Story Mode").GetComponent<Collider> (),
-//							GameObject.Find ("Arcade Mode").GetComponent<Collider> (),
-//							GameObject.Find ("Team Mode").GetComponent<Collider> (),
-//							GameObject.Find ("Royal Rumble").GetComponent<Collider> (),
-//							GameObject.Find ("Back Button O").GetComponent<Collider> ()};
-//						foreach(Collider c in submenucolliders){
-//							c.collider.enabled = true;
-//						}			
-
-						//disable parents' collider
-						foreach(Collider c in colliders){
-								//options not yet assigned
-								//c.collider.enabled = false;
-						}
-						foreach (Renderer r in renderers) {
-								if (r.name == "Options") {
-										//do nothing
-								} else {
-										//r.enabled = true;
-								}
-						}	
-						foreach (Renderer r in SinglePlayerRenderers) {
-								if (r.name == "Single Player") {
-										//do nothing
-								} else {
-										r.enabled = false;
-								}
-						}
-						foreach (Renderer r in MultiPlayerRenderers) {
-								if (r.name == "Multiplayer") {
-										//do nothing
-								} else {
-										r.enabled = false;
-								}
-						}
+						showOptionMenu();
 				} else if (isArcadeMode) {
 						Application.LoadLevel ("Single Player");
 				} else if (isRoyalRumble) {
@@ -189,6 +72,135 @@ void OnMouseUp(){
 		foreach (Renderer r in OptionsRenderers) {
 			if (r.name == "Options") {
 				r.enabled = true;
+			} else {
+				r.enabled = false;
+			}
+		}
+	}
+
+	void showSinglePlayerMenu(){
+		//show all its children, hide other's children, hide parents' renderer and collider
+		Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer> ();
+		Collider[] colliders  = new Collider[3] {GameObject.Find ("Single Player").GetComponent<Collider>(),
+			GameObject.Find ("Multiplayer").GetComponent<Collider>(),
+			GameObject.Find ("Options").GetComponent<Collider>()};
+		Renderer[] MultiPlayerRenderers = GameObject.Find ("Multiplayer").GetComponentsInChildren<Renderer> ();
+		Renderer[] OptionsRenderers = GameObject.Find ("Options").GetComponentsInChildren<Renderer> ();
+		Collider[] submenucolliders = new Collider[3] {GameObject.Find ("Story Mode").GetComponent<Collider> (),
+			GameObject.Find ("Arcade Mode").GetComponent<Collider> (),
+			GameObject.Find ("Back Button S").GetComponent<Collider> ()};
+		foreach(Collider c in submenucolliders){
+			c.collider.enabled = true;
+		}
+		//disable parents' collider
+		foreach(Collider c in colliders){
+			c.collider.enabled = false;
+		}
+		//enable children's renderer
+		foreach (Renderer r in renderers) {
+			if (r.name == "Single Player") {
+				r.enabled = false;
+			} else {
+				r.enabled = true;
+			}
+		}
+		foreach (Renderer r in MultiPlayerRenderers) {
+			if (r.name == "Multiplayer") {
+				r.enabled = false;
+			} else {
+				r.enabled = false;
+			}
+		}
+		foreach (Renderer r in OptionsRenderers) {
+			if (r.name == "Options") {
+				r.enabled = false;
+			} else {
+				r.enabled = false;
+			}
+		}
+	}
+
+	void showMultiplayerMenu(){
+		//show all its children, hide other's children
+		Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer> ();
+		Collider[] colliders  = new Collider[3] {GameObject.Find ("Single Player").GetComponent<Collider>(),
+			GameObject.Find ("Multiplayer").GetComponent<Collider>(),
+			GameObject.Find ("Options").GetComponent<Collider>()};
+		Renderer[] SinglePlayerRenderers = GameObject.Find ("Single Player").GetComponentsInChildren<Renderer> ();
+		Renderer[] OptionsRenderers = GameObject.Find ("Options").GetComponentsInChildren<Renderer> ();
+		Collider[] submenucolliders = new Collider[3] {
+			GameObject.Find ("Team Mode").GetComponent<Collider> (),
+			GameObject.Find ("Royal Rumble").GetComponent<Collider> (),
+			GameObject.Find ("Back Button M").GetComponent<Collider> ()};
+		foreach(Collider c in submenucolliders){
+			c.collider.enabled = true;
+		}
+		//disable parents' collider
+		foreach(Collider c in colliders){
+			c.collider.enabled = false;
+		}
+		foreach (Renderer r in renderers) {
+			if (r.name == "Multiplayer") {
+				r.enabled = false;
+			} else {
+				r.enabled = true;
+			}
+		}
+		foreach (Renderer r in SinglePlayerRenderers) {
+			if (r.name == "Single Player") {
+				r.enabled = false;
+			} else {
+				r.enabled = false;
+			}
+		}
+		foreach (Renderer r in OptionsRenderers) {
+			if (r.name == "Options") {
+				r.enabled = false;
+			} else {
+				r.enabled = false;
+			}
+		}
+	}
+
+	void showOptionMenu(){
+		//show all its children, hide other's children
+		Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer> ();
+		Collider[] colliders  = new Collider[3] {GameObject.Find ("Single Player").GetComponent<Collider>(),
+			GameObject.Find ("Multiplayer").GetComponent<Collider>(),
+			GameObject.Find ("Options").GetComponent<Collider>()};
+		Renderer[] SinglePlayerRenderers = GameObject.Find ("Single Player").GetComponentsInChildren<Renderer> ();
+		Renderer[] MultiPlayerRenderers = GameObject.Find ("Multiplayer").GetComponentsInChildren<Renderer> ();
+		//						Collider[] submenucolliders = new Collider[5] {GameObject.Find ("Story Mode").GetComponent<Collider> (),
+		//							GameObject.Find ("Arcade Mode").GetComponent<Collider> (),
+		//							GameObject.Find ("Team Mode").GetComponent<Collider> (),
+		//							GameObject.Find ("Royal Rumble").GetComponent<Collider> (),
+		//							GameObject.Find ("Back Button O").GetComponent<Collider> ()};
+		//						foreach(Collider c in submenucolliders){
+		//							c.collider.enabled = true;
+		//						}			
+		
+		//disable parents' collider
+		foreach(Collider c in colliders){
+			//options not yet assigned
+			//c.collider.enabled = false;
+		}
+		foreach (Renderer r in renderers) {
+			if (r.name == "Options") {
+				//do nothing
+			} else {
+				//r.enabled = true;
+			}
+		}	
+		foreach (Renderer r in SinglePlayerRenderers) {
+			if (r.name == "Single Player") {
+				//do nothing
+			} else {
+				r.enabled = false;
+			}
+		}
+		foreach (Renderer r in MultiPlayerRenderers) {
+			if (r.name == "Multiplayer") {
+				//do nothing
 			} else {
 				r.enabled = false;
 			}
