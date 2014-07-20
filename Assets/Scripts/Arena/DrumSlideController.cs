3aff1f104e8dee92726126b5b1466923663d877b
13510103 <johan.ridho@gmail.com>
2014-07-20 15:31:06 +0700

    drum slide: ganti pyramid jadi plane biasa

--- Assets/Scripts/Arena/DrumSlideController.cs
+++ Assets/Scripts/Arena/DrumSlideController.cs
@@ -10,7 +10,7 @@ public class DrumSlideController : StoneFieldController {
 		clock += Time.deltaTime;
 		
 		if (clock >= 2) {
-			float x = Random.Range(-10.0F, 10.0F);
+			float x = Random.Range(-27.0F, 25.0F);
 			Instantiate(listObstacle[0], new Vector3(x, 50, 57), Quaternion.Euler(90, -90, 0));
 			clock = 0;
 		}
