0be06da6efa5d7887ba14ca2900b9c8f51f2da9f
13510103 <johan.ridho@gmail.com>
2014-07-25 23:15:05 +0700

    rapiin posisi button skill, ganti collider missile biar ngga lag

--- Assets/Scripts/Skill/SkillController.cs
+++ Assets/Scripts/Skill/SkillController.cs
@@ -32,7 +32,7 @@ public class SkillController : MonoBehaviour {
         {
             GUIStyle style = new GUIStyle(GUI.skin.box);
             style.normal.background = skills[0].buttonSkill1;
-            if (GUI.Button(new Rect(Screen.width * 4 / 5, Screen.height * 7 / 10, Screen.width / 7, Screen.height / 8), skills[0].skillName, style))
+			if (GUI.Button(new Rect(Screen.width * 4 / 5 - Screen.width / 8 / 2, Screen.height * 7 / 10, Screen.width / 8, Screen.width / 8), skills[0].skillName, style))
             {
                 skills[0].doSkill();
             }
@@ -42,7 +42,7 @@ public class SkillController : MonoBehaviour {
         {
             GUIStyle style = new GUIStyle(GUI.skin.box);
             style.normal.background = skills[1].buttonSkill1;
-            if (GUI.Button(new Rect(Screen.width * 4 / 5, Screen.height * 5 / 10, Screen.width / 7, Screen.height / 8), skills[1].skillName, style))
+			if (GUI.Button(new Rect(Screen.width * 9 / 10 - Screen.width / 8 / 2, Screen.height * 5 / 10, Screen.width / 8, Screen.width / 8), skills[1].skillName, style))
             {
                 skills[1].doSkill();
             }
