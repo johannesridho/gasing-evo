a153ba6338ebbacdc6d5a9ce522b444a16cf3ff4
anpandu <anpandumail@gmail.com>
2014-07-26 02:05:31 +0700

    status & status burn

---
+++ Assets/Scripts/Status/Status.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Status : MonoBehaviour {
+	
+	public Gasing gasing;	
+	public float counter;
+
+	void Awake(){
+		if(!gasing)
+			gasing = GetComponent<Gasing>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Status/StatusBurn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusBurn : Status {
+	
+	public GameObject efekLedakan;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter += Time.deltaTime;
+		if (counter >= 0.75f) {
+			gasing.EPKurang(15f);		//kurangi EP tiap detik
+			counter = 0f;
+			Instantiate (efekLedakan, transform.position, Quaternion.Euler (0, 0, 0));
+		}	
+	}
+}
