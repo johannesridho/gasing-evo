141c57225eff6f11c4b95e0c5dd143847e2402ab
luthfi.chandra <luthfi.chandra@ymail.com>
2014-07-25 19:21:39 +0700

    Integrasi map baru dengan multiplayer

--- Assets/Scripts/Arena/DrumSlideController.cs
+++ Assets/Scripts/Arena/DrumSlideController.cs
@@ -7,12 +7,24 @@ public class DrumSlideController : StoneFieldController {
 		
 	void Update(){
 		base.Update ();
-		clock += Time.deltaTime;
-		
-		if (clock >= 2) {
-			float x = Random.Range(-27.0F, 25.0F);
-			Instantiate(listObstacle[0], new Vector3(x, 50, 57), Quaternion.Euler(90, -90, 0));
-			clock = 0;
-		}
+
+        if (!GamePrefs.isMultiplayer || (GamePrefs.isMultiplayer && Network.isServer))
+        {
+            clock += Time.deltaTime;
+
+            if (clock >= 2)
+            {
+                float x = Random.Range(-27.0F, 25.0F);
+                if (GamePrefs.isMultiplayer)
+                {
+                    Network.Instantiate(listObstacle[0], new Vector3(x, 50, 57), Quaternion.Euler(90, -90, 0), 10);
+                }
+                else
+                {
+                    Instantiate(listObstacle[0], new Vector3(x, 50, 57), Quaternion.Euler(90, -90, 0));
+                }
+                clock = 0;
+            }
+        }
 	}
 }
