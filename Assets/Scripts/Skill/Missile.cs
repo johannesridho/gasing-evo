f50f239337dcf650386f2f411307abfff2a716ca
luthfi.chandra <luthfi.chandra@ymail.com>
2014-07-25 11:33:17 +0700

    integrasi skill missile

--- Assets/Scripts/Skill/Missile.cs
+++ Assets/Scripts/Skill/Missile.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Missile : MonoBehaviour {
+public class Missile : Skill {
 
 	private Gasing gasing;
 	private GameObject targetEnemy;
-	public Texture2D buttonSkill1;
-	private float skillPointNeeded;		//skill point yg diperlukan
 	public GameObject prefabMissile;
 	
 	void Awake(){
+        skillName = "Missile";
 		skillPointNeeded = 20;
 		if(!gasing)
 			gasing = GetComponent<Gasing>();
@@ -31,13 +30,22 @@ public class Missile : MonoBehaviour {
 		GUIStyle style = new GUIStyle (GUI.skin.box);
 		style.normal.background = buttonSkill1;
 		
-		if (GUI.Button (new Rect (Screen.width * 4 / 5, Screen.height * 6 / 10, Screen.width / 7, Screen.width / 7), "", style) && gasing.getSP()>skillPointNeeded) {
+        //if (GUI.Button (new Rect (Screen.width * 4 / 5, Screen.height * 6 / 10, Screen.width / 7, Screen.width / 7), "", style) && gasing.getSP()>skillPointNeeded) {
 
-			if(targetEnemy){
-				Instantiate(prefabMissile, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.LookRotation(targetEnemy.transform.position-transform.position - new Vector3(0,-90,0)));
-//				Instantiate(prefabMissile, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.Euler(0, 0, 0));
-			gasing.SPKurang(skillPointNeeded);		//kurangi skillpoint gasing		
-			}
-		}
+			
+        //}
 	}
+
+    public override void doSkill()
+    {
+        if (gasing.getSP() > skillPointNeeded)
+        {
+            if (targetEnemy)
+            {
+                Instantiate(prefabMissile, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.LookRotation(targetEnemy.transform.position - transform.position - new Vector3(0, -90, 0)));
+                //				Instantiate(prefabMissile, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.Euler(0, 0, 0));
+                gasing.SPKurang(skillPointNeeded);		//kurangi skillpoint gasing		
+            }
+        }
+    }
 }
