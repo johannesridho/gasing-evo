07c272e09433d70f3ed43976b27f5aa37b2a2bdc
13510103 <johan.ridho@gmail.com>
2014-07-26 01:13:51 +0700

    prefab gasing langsung diload di code, pindahin folder prefab gasing

--- Assets/Scripts/StoneFieldController.cs
+++ Assets/Scripts/StoneFieldController.cs
@@ -8,9 +8,7 @@ public class StoneFieldController : MonoBehaviour
     public GameObject musuh;
     public Gasing gasingPemain;
     public Gasing gasingMusuh;
-    private int jumlahMusuh;
-    public GameObject[] listPemain;
-    public GameObject[] listMusuh;
+    private int jumlahMusuh;        
 	public GameObject[] listObstacle;
 
     protected void Awake()
@@ -23,105 +21,117 @@ public class StoneFieldController : MonoBehaviour
         {
     		jumlahMusuh = Utilities.howManyGasingRoyal-1;
             if (!pemain)
-            {
-                if (listPemain.Length != 0)
-                {
-					int nomorGasing = 0;
-					switch (Utilities.playerGasing){
-						case "arjuna":
-							nomorGasing = 0;
-							break;
-						case "srikandi":
-							nomorGasing = 1;
-							break;
-						case "prototype":
-							nomorGasing = 2;
-							break;
-						default:
-							nomorGasing = 0;
-							break;
-					}
+            {                
+				Object objekGasing;
+				switch (Utilities.playerGasing){
+					case "arjuna":
+						objekGasing =  Resources.Load("Prefab/Prefab Gasing/Arjuna");
+						break;
+					case "srikandi":
+						objekGasing =  Resources.Load("Prefab/Prefab Gasing/Srikandi");
+						break;
+					case "prototype":
+						objekGasing =  Resources.Load("Prefab/Prefab Gasing/Prototype");
+						break;
+					case "dipati":
+						objekGasing =  Resources.Load("Prefab/Prefab Gasing/Dipati");
+						break;
+					case "jalaprang":
+						objekGasing =  Resources.Load("Prefab/Prefab Gasing/Jalaprang");
+						break;
+				default:
+						objekGasing =  Resources.Load("Prefab/Prefab Gasing/Arjuna");
+						break;
+				}
 
-                    pemain = (GameObject)Instantiate(listPemain[nomorGasing], new Vector3(0, 1, -15), Quaternion.Euler(270, 0, 0));		//hidupin gasing, pilih prefab
-                    pemain.name = "Pemain";
-					pemain.renderer.material.color = Color.blue;
-                }
+				pemain = (GameObject)Instantiate(objekGasing, new Vector3(0, 1, -15), Quaternion.Euler(270, 0, 0));		//hidupin gasing, pilih prefab
+           		pemain.name = "Pemain";
+                
                 if (!gasingPemain)
                 {
                     gasingPemain = pemain.GetComponent<Gasing>();
                 }
             }
             if (!musuh)
-            {
-                if (listMusuh.Length != 0)
-                {
-					int nomorGasing = 0;
-					switch (Utilities.enemy1){
+            {                
+				Object objekGasing2;
+				switch (Utilities.enemy1){
+				case "arjuna":
+					objekGasing2 =  Resources.Load("Prefab/Prefab Gasing/Arjuna_Musuh");
+					break;
+				case "srikandi":
+					objekGasing2 =  Resources.Load("Prefab/Prefab Gasing/Srikandi_Musuh");
+					break;
+				case "prototype":
+					objekGasing2 =  Resources.Load("Prefab/Prefab Gasing/Prototype_Musuh");
+					break;
+				case "dipati":
+					objekGasing2 =  Resources.Load("Prefab/Prefab Gasing/Dipati_Musuh");
+					break;
+				case "jalaprang":
+					objekGasing2 =  Resources.Load("Prefab/Prefab Gasing/Jalaprang_Musuh");
+					break;
+				default:
+					objekGasing2 =  Resources.Load("Prefab/Prefab Gasing/Arjuna_Musuh");
+					break;
+				}
+				musuh = (GameObject) Instantiate(objekGasing2, new Vector3(0, 1, 10), Quaternion.Euler(270, 0, 0));
+			    
+        
+				if(jumlahMusuh > 1){
+					Object objekGasing3;
+					switch (Utilities.enemy2){
 					case "arjuna":
-						nomorGasing = 0;
+						objekGasing3 =  Resources.Load("Prefab/Prefab Gasing/Arjuna_Musuh");
 						break;
 					case "srikandi":
-						nomorGasing = 1;
+						objekGasing3 =  Resources.Load("Prefab/Prefab Gasing/Srikandi_Musuh");
 						break;
 					case "prototype":
-						nomorGasing = 2;
+						objekGasing3 =  Resources.Load("Prefab/Prefab Gasing/Prototype_Musuh");
+						break;
+					case "dipati":
+						objekGasing3 =  Resources.Load("Prefab/Prefab Gasing/Dipati_Musuh");
+						break;
+					case "jalaprang":
+						objekGasing3 =  Resources.Load("Prefab/Prefab Gasing/Jalaprang_Musuh");
 						break;
 					default:
-						nomorGasing = 0;
+						objekGasing3 =  Resources.Load("Prefab/Prefab Gasing/Arjuna_Musuh");
 						break;
 					}
-                    Instantiate(listMusuh[nomorGasing], new Vector3(0, 1, 10), Quaternion.Euler(270, 0, 0));
-                    musuh = listMusuh[nomorGasing];
-                
-					if(jumlahMusuh > 1){
-						int nomorGasingMsh2 = 0;
-						switch (Utilities.enemy2){
-						case "arjuna":
-							nomorGasingMsh2 = 0;
-							break;
-						case "srikandi":
-							nomorGasingMsh2 = 1;
-							break;
-						case "prototype":
-							nomorGasingMsh2 = 2;
-							break;
-						default:
-							nomorGasingMsh2 = 0;
-							break;
-						}
-						Instantiate(listMusuh[nomorGasingMsh2], new Vector3(-5, 1, 10), Quaternion.Euler(270, 0, 0));
-					}
-					if(jumlahMusuh > 2){
-						//karena layar ga muat buat pilih gasing, klo musuh lebih dari 2 , musuh diclone dari objek musuh pertama
-	                    for (int i = 2; i < jumlahMusuh; i++)
-	                    {
-	                        if (i <= 4)
-	                        {
-	                            Instantiate(musuh, new Vector3(i * (-5), 1, 10), Quaternion.Euler(270, 0, 0));
-	                        }
-	                        else if (i <= 7)
-	                        {
-	                            Instantiate(musuh, new Vector3((i - 4) * 5, 1, 10), Quaternion.Euler(270, 0, 0));
-	                        }
-	                        else if (i <= 11)
-	                        {
-	                            Instantiate(musuh, new Vector3((i - 7) * 5, 1, -10), Quaternion.Euler(270, 0, 0));
-	                        }
-	                        else if (i <= 14)
-	                        {
-	                            Instantiate(musuh, new Vector3((i - 11) * (-5), 1, -10), Quaternion.Euler(270, 0, 0));
-	                        }
-	                        else if (i <= 17)
-	                        {
-	                            Instantiate(musuh, new Vector3((i - 14) * 5, 1, -15), Quaternion.Euler(270, 0, 0));
-	                        }
-	                        else if (i <= 20)
-	                        {
-	                            Instantiate(musuh, new Vector3((i - 17) * (-5), 1, -15), Quaternion.Euler(270, 0, 0));
-	                        }
-	                    }
-	                }
-				}//end if listmusuh tidak kosong 
+					Instantiate(objekGasing3, new Vector3(-5, 1, 10), Quaternion.Euler(270, 0, 0));
+				}
+				if(jumlahMusuh > 2){
+					//karena layar ga muat buat pilih gasing, klo musuh lebih dari 2 , musuh diclone dari objek musuh pertama
+                    for (int i = 2; i < jumlahMusuh; i++)
+                    {
+                        if (i <= 4)
+                        {
+                            Instantiate(musuh, new Vector3(i * (-5), 1, 10), Quaternion.Euler(270, 0, 0));
+                        }
+                        else if (i <= 7)
+                        {
+                            Instantiate(musuh, new Vector3((i - 4) * 5, 1, 10), Quaternion.Euler(270, 0, 0));
+                        }
+                        else if (i <= 11)
+                        {
+                            Instantiate(musuh, new Vector3((i - 7) * 5, 1, -10), Quaternion.Euler(270, 0, 0));
+                        }
+                        else if (i <= 14)
+                        {
+                            Instantiate(musuh, new Vector3((i - 11) * (-5), 1, -10), Quaternion.Euler(270, 0, 0));
+                        }
+                        else if (i <= 17)
+                        {
+                            Instantiate(musuh, new Vector3((i - 14) * 5, 1, -15), Quaternion.Euler(270, 0, 0));
+                        }
+                        else if (i <= 20)
+                        {
+                            Instantiate(musuh, new Vector3((i - 17) * (-5), 1, -15), Quaternion.Euler(270, 0, 0));
+                        }
+                    }
+                }				
             }//end if !musuh
         }//end if single player
 
