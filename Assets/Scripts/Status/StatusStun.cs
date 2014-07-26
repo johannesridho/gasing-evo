205c4c4be6f6428f0d5201ac1f5bfdc3f0456351
anpandu <anpandumail@gmail.com>
2014-07-26 17:01:01 +0700

    status stun

---
+++ Assets/Scripts/Status/StatusStun.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusStun : Status {
+	
+	void Start () {
+		aksi();
+	}
+	
+	void aksi () {
+		gasing.speedMaxChange(0.001f);
+	}
+}
