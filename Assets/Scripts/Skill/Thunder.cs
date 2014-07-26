687ee1ec2640b57fc8d969bffa1662b8f6850a31
13510103 <johan.ridho@gmail.com>
2014-07-26 15:34:24 +0700

    tambah skill thunder buat dipati

---
+++ Assets/Scripts/Skill/Thunder.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thunder : Skill {
+
+	private Gasing gasing;
+	private Object prefab;
+	private GameObject targetEnemy;
+	private Gasing gasingEnemy;
+	
+	void Awake(){
+		skillName = "Thunder";
+		skillPointNeeded = 20;
+		damageInflicted = 25;
+		if(!gasing)
+			gasing = GetComponent<Gasing>();
+		prefab = Resources.Load("Prefab/Prefab Obstacle/Thunder");
+		targetEnemy = GameObject.FindGameObjectWithTag("Enemy");
+		if(targetEnemy)
+			gasingEnemy = targetEnemy.GetComponent<Gasing> ();
+	}
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (!targetEnemy) {
+			targetEnemy = GameObject.FindGameObjectWithTag("Enemy");
+			if (!gasingEnemy) {
+				gasingEnemy = targetEnemy.GetComponent<Gasing> ();
+			}
+		}
+	}
+	
+	public override void doSkill()
+	{
+		if (gasing.getSP() > skillPointNeeded && targetEnemy)
+		{
+			Instantiate(prefab, targetEnemy.transform.position, Quaternion.Euler(270, 0, 0));			
+			Instantiate(prefab, targetEnemy.transform.position, Quaternion.Euler(270, 0, 0));
+			Instantiate(prefab, targetEnemy.transform.position, Quaternion.Euler(270, 0, 0));
+			gasingEnemy.EPKurang(damageInflicted);
+			gasing.SPKurang(skillPointNeeded);		//kurangi skillpoint gasing		
+		}
+	}
+}
