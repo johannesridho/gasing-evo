0184a771a73895f7c12d5785f2cfa2b29db7435f
13510103 <johan.ridho@gmail.com>
2014-07-30 17:50:15 +0700

    tambah skill meteor buat srikandi

---
+++ Assets/Scripts/Obstacle/MeteorScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorScript : MonoBehaviour {
+
+	private float clock;
+	private float damageInflicted;
+	
+	void Awake() {
+		clock = 0f;
+		damageInflicted = 5;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update(){
+		clock += Time.deltaTime;
+		if (clock >= 15) {
+			Destroy(this.gameObject);
+		}
+	}
+
+//	private ParticleSystem.CollisionEvent[] collisionEvents = new ParticleSystem.CollisionEvent[16];
+	void OnParticleCollision(GameObject objek) {
+//		Debug.Log (transform.position);
+		Collider[] colls = Physics.OverlapSphere (new Vector3(transform.position.x, 1, transform.position.z), 7);
+		foreach (Collider col in colls){
+			if (col.tag == "Enemy") {
+				col.gameObject.GetComponent<Gasing>().EPKurang(10);
+			}		
+		}
+	}
+}
---
+++ Assets/Scripts/Skill/Meteor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meteor : Skill {
+
+	private Gasing gasing;
+	private Object prefab;
+	private GameObject targetEnemy;
+	private Gasing gasingEnemy;
+	
+	void Awake(){
+		skillName = "Meteor";
+		skillPointNeeded = 20;
+		if(!gasing)
+			gasing = GetComponent<Gasing>();
+		prefab = Resources.Load("Prefab/Prefab Obstacle/Meteor Storm");
+	}
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	public override void doSkill()
+	{
+		if (gasing.getSP() > skillPointNeeded)
+		{
+			targetEnemy = findNearestEnemy();		//cari musuh terdekat
+			if (targetEnemy) {
+				gasingEnemy = targetEnemy.GetComponent<Gasing> ();			
+				Instantiate(prefab, new Vector3(targetEnemy.transform.position.x, targetEnemy.transform.position.y+15, targetEnemy.transform.position.z), Quaternion.Euler(0, 0, 0));			
+				gasing.SPKurang(skillPointNeeded);		//kurangi skillpoint gasing		
+			}
+		}
+	}
+}
