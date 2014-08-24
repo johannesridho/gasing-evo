ef3e0edef545aea93cf2797b164cbea47f28f6ff
anpandu <anpandumail@gmail.com>
2014-08-25 00:20:04 +0700

    ulti empty soul di colonix

---
+++ Assets/Scripts/Skill/UltiEmptySoul.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UltiEmptySoul : Skill {
+	
+	private Gasing gasing;
+	public GameObject[] targetEnemies;
+	private Object prefabMissile;
+	
+	void Awake(){
+		skillName = "Empty Soul";
+		skillPointNeeded = 20;
+		if(!gasing)
+			gasing = GetComponent<Gasing>();
+		targetEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+	}
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		//		if (!targetEnemy) {
+		//
+		//		}
+	}
+	
+	public override void doSkill()
+	{
+		targetEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+		if (gasing.getSP() > skillPointNeeded)
+		{
+			if (targetEnemies.Length > 0)
+			{
+				if (GamePrefs.isMultiplayer)
+				{
+					
+				}
+				else
+				{
+					foreach (GameObject targetEnemy in targetEnemies) {
+						Gasing targetEnemyGasing = targetEnemy.GetComponent<Gasing>();
+						Debug.Log(targetEnemy.GetComponent<StatusController>());
+						if (targetEnemyGasing) {
+							targetEnemyGasing.EPKurang(targetEnemyGasing.energiPoint*0.5f);
+						}
+					}
+					//					while (targetEnemySC!=null) {
+					//						targetEnemySC = targetEnemy.GetComponent<StatusController>();
+					//					}
+					//					gasing.SPKurang(skillPointNeeded);		//kurangi skillpoint gasing		
+				}
+			}
+		}
+	}
+}
