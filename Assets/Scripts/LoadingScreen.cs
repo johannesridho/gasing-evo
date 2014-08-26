a860018b0ed49af6209bd52083dbce864c98b24a
luthfi.chandra <luthfi.chandra@ymail.com>
2014-08-26 18:00:58 +0700

    benerin LoadingScreen

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -6,7 +6,15 @@ public class LoadingScreen : MonoBehaviour
 
     public Texture2D texture;
     static LoadingScreen instance;
-
+    private bool isLoading = false;
+    #region untuk GUI
+    private float guiRatioX;
+    private float guiRatioY;
+    private float sWidth;
+    private float sHeight;
+    private Vector3 GUIsF;
+    private int sizegui;
+    #endregion
     void Awake()
     {
         if (instance)
@@ -22,10 +30,31 @@ public class LoadingScreen : MonoBehaviour
         DontDestroyOnLoad(this);
     }
 
+    void Start()
+    {
+        //get the screen's width
+        sWidth = Screen.width;
+        sHeight = Screen.height;
+        //calculate the rescale ratio
+        guiRatioX = sWidth / 1280;
+        guiRatioY = sHeight / 720;
+        //create a rescale Vector3 with the above ratio
+        GUIsF = new Vector3(guiRatioX, guiRatioY, 1);
+    }
+
     void Update()
     {
         if (!Application.isLoadingLevel)
             hide();
+    }
+
+    void OnGUI()
+    {
+        if (isLoading)
+        {
+            GUI.matrix = Matrix4x4.TRS(new Vector3(GUIsF.x, GUIsF.y, 0), Quaternion.identity, GUIsF);
+            GUI.DrawTexture(new Rect(0, 0, 1280, 720), texture);
+        }
     }
 
     public static void show()
@@ -34,7 +63,8 @@ public class LoadingScreen : MonoBehaviour
         {
             return;
         }
-        instance.guiTexture.enabled = true;
+        //instance.guiTexture.enabled = true;
+        instance.isLoading = true;
     }
 
     public static void hide()
@@ -43,7 +73,8 @@ public class LoadingScreen : MonoBehaviour
         {
             return;
         }
-        instance.guiTexture.enabled = false;
+        //instance.guiTexture.enabled = false;
+        instance.isLoading = false;
     }
 
     static bool InstanceExists()
