e2e2aa4c1444399205046f445c165ccae0f908d3
13510103 <johan.ridho@gmail.com>
2014-08-29 12:17:28 +0700

    gedein size gladiator, tambahin health gasing, tambahin nilai regenerate skillPoint

--- Assets/Scripts/Gasing.cs
+++ Assets/Scripts/Gasing.cs
@@ -68,7 +68,7 @@ public class Gasing : MonoBehaviour {
 	void Update () {
 		if (!paused && Time.timeScale != 0) {
 			EPKurang(0.05f);		//kurangi EP tiap detik
-			SPTambah (0.01f);		//tambah SP tiap detik
+			SPTambah (0.1f);		//tambah SP tiap detik
 			if(energiPoint <=0 || transform.position.y <= (-10)){
 				//gasing berhenti
 				//Debug.Log("wah");
